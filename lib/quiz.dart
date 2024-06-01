b7ce4408e2cc4572ef802a90b93b53a00852bd8a
Leevi Alanen <leevi.alanen1@gmail.com>
2024-06-01 16:25:33 +0300

    Initial commit with few screens built

---
+++ lib/quiz.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 158, 4, 189),
+                  Color.fromARGB(255, 61, 3, 161),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
+      ),
+    );
+  }
+}
